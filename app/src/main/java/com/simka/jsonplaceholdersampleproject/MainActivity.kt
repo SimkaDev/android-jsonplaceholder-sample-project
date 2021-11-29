d89ffb5961dae8cef7d01b07a3143bb57b39f627
simka <simka.dev@gmail.com>
2021-11-29 14:47:07 +0100

    Initial commit

---
+++ app/src/main/java/com/simka/jsonplaceholdersampleproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.simka.jsonplaceholdersampleproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
